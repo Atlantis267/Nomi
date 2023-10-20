1e9960803cbb737ef20de5a6b2c0686cb583e72f
jasontseng9050@gmail.com <jasontseng9050@gmail.com>
2023-10-21 01:13:18 +0800

    UI鼠標偵測.Test

---
+++ Assets/Script/UI/UITest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class UITest : MonoBehaviour
+{
+    public Button button;
+    [SerializeField] private bool isMouesEnter;
+    private CanvasGroup buttonTransition;
+    void Start()
+    {
+        isMouesEnter = false;
+        buttonTransition = button.GetComponent<CanvasGroup>();
+    }
+    void Update()
+    {
+        isMouesEnter = EventSystem.current.IsPointerOverGameObject();
+        if (isMouesEnter) 
+        {
+            Debug.Log("12345");
+        }
+    }
+}
