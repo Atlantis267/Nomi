ddb4e35edead394a40c2fefd8607b2958a26227d
jasontseng9050@gmail.com <jasontseng9050@gmail.com>
2023-11-01 14:03:10 +0800

    UI選擇效果

--- Assets/Script/UI/UI_LoadGame.cs
+++ Assets/Script/UI/UI_LoadGame.cs
@@ -1,20 +1,16 @@
-ï»¿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-
-public class UITest : MonoBehaviour
+public class UI_LoadGame : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 {
     [SerializeField] private bool isPointerIn;
     [SerializeField] private float movement = 25.0f;
-    private CanvasGroup buttonTransition; //æŽ§åˆ¶UIç™¼å…‰
+    private CanvasGroup buttonTransition; //±±¨îUIµo¥ú
     private Vector3 startPosition;
     private Vector3 endPosition;
-    //[SerializeField] private float time = 0.5f;
     [SerializeField] private float speed = 3.0f;
-    //private float counter;
     void Start()
     {
         isPointerIn = false;
@@ -22,12 +18,23 @@ public class UITest : MonoBehaviour
         startPosition = transform.position;
         endPosition = startPosition + new Vector3(movement, 0, 0);
     }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter.CompareTag("LoadGame"))
+        {
+            isPointerIn = true;
+        }
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter.CompareTag("LoadGame"))
+        {
+            isPointerIn = false;
+        }
+    }
     void Update()
     {
-        //counter += Time.deltaTime;
-        //float timesUp = counter / time;
-        isPointerIn = EventSystem.current.IsPointerOverGameObject();
-        if (isPointerIn) 
+        if (isPointerIn)
         {
             transform.position = Vector3.Lerp(transform.position, endPosition, Time.deltaTime * speed);
         }
