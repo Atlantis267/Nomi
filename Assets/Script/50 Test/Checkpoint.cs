b5425540ed743a68aeb322c00dd40c1d5f3b91d2
ltw422 <lautszwai282@gmail.com>
2023-12-21 22:44:13 +0800

    CheckPoint Fix

--- Assets/Script/50 Test/Checkpoint.cs
+++ Assets/Script/50 Test/Checkpoint.cs
@@ -13,7 +13,7 @@ public class Checkpoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        vectorPoint = player.transform.position;
     }
 
     // Update is called once per frame
