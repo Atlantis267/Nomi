a42d02285aa5fd9760514a90638f6c4f821dd1b3
jasontseng9050@gmail.com <jasontseng9050@gmail.com>
2024-03-05 23:09:19 +0800

    SettingScene

---
+++ Assets/Script/UI/SettingMenu/UI_Jump.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UI_Jump : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private bool isPointerIn;
+    [SerializeField] private float movement = 30.0f;
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+    [SerializeField] private float speed = 4.5f;
+    void Start()
+    {
+        isPointerIn = false;
+        startPosition = transform.position;
+        endPosition = startPosition + new Vector3(movement, 0, 0);
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter.CompareTag("Jump"))
+        {
+            isPointerIn = true;
+        }
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerEnter.CompareTag("Jump"))
+        {
+            isPointerIn = false;
+        }
+    }
+
+    void Update()
+    {
+        if (isPointerIn)
+        {
+            transform.position = Vector3.Lerp(transform.position, endPosition, Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.position = startPosition;
+        }
+    }
+}
