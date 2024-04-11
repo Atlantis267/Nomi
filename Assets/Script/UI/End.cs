5d212cf89b9a85b606e890cd9449b12ac7a488e9
jasontseng9050@gmail.com <jasontseng9050@gmail.com>
2024-04-11 23:33:52 +0800

    Victory

--- Assets/Script/UI/End.cs
+++ Assets/Script/UI/End.cs
@@ -6,20 +6,52 @@ using Cinemachine;
 public class End : MonoBehaviour
 {
     private Animator victoryTrans;
+    private Animator endPanelTrans;
+    private Animator leftTreeTrans;
+    private Animator rightTreeTrans;
+    private Animator grassTrans;
+    [SerializeField] private bool checking;
     public CinemachineInputProvider cinemachineInput;
     void Start()
     {
-        victoryTrans = gameObject.GetComponent<Animator>();
+        
+        victoryTrans = GameObject.Find("Victory").GetComponent<Animator>();
+        endPanelTrans = GameObject.Find("EndPanel").GetComponent<Animator>();
+        leftTreeTrans = GameObject.Find("LeftTree").GetComponent<Animator>();
+        rightTreeTrans = GameObject.Find("RightTree").GetComponent<Animator>();
+        grassTrans = GameObject.Find("Grass").GetComponent<Animator>();
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             victoryTrans.SetTrigger("Start");
+            endPanelTrans.SetTrigger("Start");
+            leftTreeTrans.SetTrigger("Start");
+            rightTreeTrans.SetTrigger("Start");
+            grassTrans.SetTrigger("Start");
             Time.timeScale = 0f;
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.Confined;
             cinemachineInput.enabled = false;
         }
     }
+    /*
+    private void Updat()
+    {
+        if (checking)
+        {
+            victoryTrans.SetTrigger("Start");
+            endPanelTrans.SetTrigger("Start");
+            leftTreeTrans.SetTrigger("Start");
+            rightTreeTrans.SetTrigger("Start");
+            grassTrans.SetTrigger("Start");
+            Time.timeScale = 0f;
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            cinemachineInput.enabled = false;
+        }
+    }
+    */
 }
