f87cdd40938216f6ef67c6724625c19094946904
jasontseng9050@gmail.com <jasontseng9050@gmail.com>
2024-05-17 16:19:00 +0800

    3333

--- Assets/Script/UI/MainMenu/ChangeScene.cs
+++ Assets/Script/UI/MainMenu/ChangeScene.cs
@@ -6,28 +6,46 @@ using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour
 {
-    private Animator flowerAnime;
+    [SerializeField] private Animator flowerAnime;
+    [SerializeField] private Animator transAnime;
     private float transWaitTime = 1.0f;
-    void Start()
+    private float transWaitTime2 = 1.8f;
+    void Awake()
     {
-        flowerAnime = gameObject.GetComponent<Animator>();
+        //flowerAnime = GetComponentInChildren<Animator>();
+        //transAnime = gameObject.GetComponent<Animator>();
     }
     IEnumerator WaitAnimation() 
     {
-        PlayAnimation();
+        PlayFlowerAnimation();
         yield return new WaitForSeconds(transWaitTime);
         SceneManager.LoadScene("Loading");
     }
-    public void PlayAnimation() 
+    public void PlayFlowerAnimation() 
     {
         flowerAnime.SetTrigger("Start");
     }
-    public void ButtonClick() 
+    public void NewGameButtonClick() 
     {
         StartCoroutine(WaitAnimation());
     }
-    void Update()
+    IEnumerator WaitTransfromAnimation()
     {
-      
+        PlayAnimation();
+        yield return new WaitForSeconds(transWaitTime2);
+        SceneManager.LoadScene("Information");
+    }
+    public void PlayAnimation()
+    {
+        transAnime.SetTrigger("Start");
+    }
+    public void SettingClick()
+    {
+        StartCoroutine(WaitTransfromAnimation());
+    }
+    public void QuitClick()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
